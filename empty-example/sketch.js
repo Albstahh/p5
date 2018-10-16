83a44186b5c49ea91b118ec11b2d431f582164e2
Albert Viilik <42554671+Albstahh@users.noreply.github.com>
2018-10-16 01:21:20 +0100

    Initial Commit

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,10 +1,20 @@
+let width = 600;
+let height = 400;
+let radius = 100;
+let gravity = 10;
+
 function setup() {
-  // put setup code here
+    // put setup code here
+    createCanvas(width, height);
     background(51);
 
 }
 
 function draw() {
-  // put drawing code here
+    // put drawing code here
+    ball((width) / 2, (height) / 2);
+}
 
+function ball(x, y) {
+    ellipse(x, y, radius);
 }
